0999e5b3b298cabbf6ef716a3b7fe81f96ab3743
JUAN CARLOS <JUAN CARLOS@LAPTOP-NF1JBM48>
2024-02-06 20:01:27 -0600

    Comentario para ver que hace cada cosa

--- src/cursos/Cursos.java
+++ src/cursos/Cursos.java
@@ -16,16 +16,16 @@ public class Cursos {
         switch(Curso_tomar)
         {
             case 1:
-                JOptionPane.showMessageDialog(null,"El Curso selecionado es Windows y su precio es de $150");
+                JOptionPane.showMessageDialog(null,"El Curso selecionado es Windows \n  su precio es de:  $150");
             break;
             case 2:
-                JOptionPane.showMessageDialog(null,"El Curso selecionado es Excel y su precio es de $300");
+                JOptionPane.showMessageDialog(null,"El Curso selecionado es Excel \n su precio es de: $300");
             break;
             case 3:
-                JOptionPane.showMessageDialog(null,"El Curso selecionado es Diseño y su precio es de $200");
+                JOptionPane.showMessageDialog(null,"El Curso selecionado es Diseño\n su precio es de: $200");
             break;
             case 4:
-                JOptionPane.showMessageDialog(null,"El Curso selecionado es Sistemas operativos y su precio es de $250");
+                JOptionPane.showMessageDialog(null,"El Curso selecionado es Sistemas operativos \n su precio es de: $250");
             break;
             default:
                 JOptionPane.showMessageDialog(null, "No has selecionado ningun Curso");
